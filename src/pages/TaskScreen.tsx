5cfd6a97531533ec7c0bbca9b055b802b71776ac
jecinta2003 <onyemaobijecintaugochi@gmail.com>
2023-08-07 17:44:24 +0100

    jecinta

--- src/pages/TaskScreen.tsx
+++ src/pages/TaskScreen.tsx
@@ -1,10 +1,36 @@
 
-
+import React from "react"
+import styled from "styled-components";
 
 const TaskScreen = ()=>{
     return(
-        <div>TaskScreen</div>
+        <div>
+        <Container>
+        <Main>
+        <CardHolder>
+        <Card>
+        <Profile></Profile>
+        <Name>Jecinta</Name>
+        <Task></Task>
+        <Time></Time>
+        <Button></Button>
+        </Card>
+        </CardHolder>
+        </Main>
+        </Container>
+        </div>
     )
 }
 
 export default TaskScreen
+
+
+const Button = styled.div``;
+const Time = styled.div``;
+const Task = styled.div``;
+const Name = styled.div``;
+const Profile = styled.div``;
+const Card = styled.div``;
+const CardHolder = styled.div``;
+const Main = styled.div``;
+const Container = styled.div``;
